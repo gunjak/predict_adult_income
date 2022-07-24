550b081de818f593145e3521a26dafb17ee0a75f
gunjak <gunjakjaiswal999@gmail.com>
2022-07-24 16:25:52 +0530

    first commit

---
+++ income_prediction/component/data_ingestion.py
@@ -0,0 +1,71 @@
+from operator import index
+from tkinter import E
+from income_prediction.logger import logging
+from income_prediction.exception import IncomeException
+from income_prediction.entity.entity_config import DataIngestionConfig
+import sys,os
+import pandas as pd
+from income_prediction.entity.artifact_entity import DataIngestionArtifact
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+class DataIngestion:
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
+        try:
+            logging.info(f"{'>>'*20}Data Ingestion log started.{'<<'*20} ")
+            self.data_ingestion_config=data_ingestion_config
+        except Exception as e:
+            raise IncomeException(e,sys) from e
+    def download_url_data(self)->str:
+        try:
+            download_url=self.data_ingestion_config.dataset_url
+            raw_data_dir=self.data_ingestion_config.raw_dir
+            if os.path.exists(raw_data_dir):
+                os.remove(raw_data_dir)
+
+            os.makedirs(raw_data_dir,exist_ok=True)
+            file_path=self.data_ingestion_config.raw_data_file
+            df=pd.read_csv(download_url)
+            df.columns=['age','workclass','fnlwgt','education','education_num','marital_status',
+                        'occupation','relationship','race','sex','capital_gain','capital_loss','hours_per_week',
+                        'native_country','wages']
+            df.drop(columns=df.columns[3],axis=1,inplace=True)
+            df[df==' ?']=np.nan
+            df.to_csv(file_path,index=False)
+            logging.info(f'download data from {download_url} to {file_path}')
+            return file_path
+        except Exception as e:
+            raise IncomeException(e,sys) from e
+        
+    def split_data_as_train_test(self)->DataIngestionArtifact:
+        try:
+            adult_income_data=pd.read_csv(self.data_ingestion_config.raw_data_file)
+            file=os.path.basename(self.data_ingestion_config.raw_data_file)
+            train_set=None
+            test_set=None
+            train_set,test_set=train_test_split(adult_income_data,test_size=0.25,stratify=adult_income_data.iloc[:,-1],random_state=42)
+            train_file_path=os.path.join(self.data_ingestion_config.ingested_train_dir,file)
+            test_file_path=os.path.join(self.data_ingestion_config.ingested_test_dir,file)
+            
+            if train_set is not None:
+                os.makedirs(self.data_ingestion_config.ingested_train_dir,exist_ok=True)
+                logging.info(f'Exporting training data to file [{train_file_path}]')
+                train_set.to_csv(train_file_path,index=False)
+            if test_set is not None:
+                os.makedirs(self.data_ingestion_config.ingested_test_dir,exist_ok=True)
+                logging.info(f"Exporting testing dataset to file :[{test_file_path}]")
+                test_set.to_csv(test_file_path,index=False)
+            data_ingestion_artifact=DataIngestionArtifact(train_file_path=train_file_path,
+                                                        test_file_path=test_file_path,
+                                                        is_ingested=True,
+                                                        message=f'Data ingestion completed successfully')
+            logging.info(f'Data ingestion artifact: [{data_ingestion_artifact}]')
+            return data_ingestion_artifact        
+        except Exception as e:
+            raise IncomeException(e,sys) from e 
+    def initiate_data_ingestion(self)->DataIngestionArtifact:
+        try:
+            data_file_path=self.download_url_data()  
+            return self.split_data_as_train_test()
+        except Exception as e:
+            raise IncomeException(e,sys) from e
---
+++ income_prediction/entity/income_predictor.py
@@ -0,0 +1,102 @@
+import os
+import sys
+
+from income_prediction.exception import IncomeException
+from income_prediction.util.util import load_object
+
+import pandas as pd
+
+
+class IncomeData:
+
+    def __init__(self,
+                age: int,
+                workclass: str,
+                 fnlwgt: int ,
+                education_num: int, 
+                marital_status: str,
+                occupation: str,
+                relationship: str,
+                race: str,
+                sex: str,
+                capital_gain: int, 
+                capital_loss: int, 
+                hours_per_week: int, 
+                native_country: str,
+                wages: str =None
+                 ):
+        try:
+            self.age = age
+            self.workclass = workclass
+            self.fnlwgt = fnlwgt
+            self.education_num = education_num
+            self.marital_status = marital_status
+            self.occupation = occupation
+            self.relationship = relationship
+            self.race= race
+            self.sex = sex
+            self.capital_gain= capital_gain
+            self.capital_loss=capital_loss
+            self.hours_per_week=hours_per_week
+            self.native_country=native_country
+            self.wages=wages
+
+        except Exception as e:
+            raise IncomeException(e, sys) from e
+
+    def get_income_input_data_frame(self):
+
+        try:
+            income_input_dict = self.get_income_data_as_dict()
+            return pd.DataFrame(income_input_dict)
+        except Exception as e:
+            raise IncomeException(e, sys) from e
+
+    def get_income_data_as_dict(self):
+        try:
+            input_data = {
+                "age": [self.age],
+                "workclass": [self.workclass],
+                "fnlwgt": [self.fnlwgt],
+                "education_num": [self.education_num],
+                "marital_status": [self.marital_status],
+                "occupation": [self.occupation],
+                "relationship": [self.relationship],
+                "race": [self.race],
+                "sex": [self.sex],
+                'capital_gain':[self.capital_gain],
+                'capital_loss':[self.capital_loss],
+                'hours_per_week': [self.hours_per_week],
+                'native_country' :[self.native_country]
+                }
+            return input_data
+        except Exception as e:
+            raise IncomeException(e, sys)
+
+
+class IncomePredictor:
+
+    def __init__(self, model_dir: str):
+        try:
+            self.model_dir = model_dir
+        except Exception as e:
+            raise IncomeException(e, sys) from e
+
+    def get_latest_model_path(self):
+        try:
+            folder_name = list(map(int, os.listdir(self.model_dir)))
+            latest_model_dir = os.path.join(self.model_dir, f"{max(folder_name)}")
+            file_name = os.listdir(latest_model_dir)[0]
+            latest_model_path = os.path.join(latest_model_dir, file_name)
+            return latest_model_path
+        except Exception as e:
+            raise IncomeException(e, sys) from e
+
+    def predict(self, X):
+        try:
+            model_path = self.get_latest_model_path()
+            model = load_object(file_path=model_path)
+            prediction_value = model.predict(X)
+            return prediction_value
+        except Exception as e:
+            raise IncomeException(e, sys) from e
